d306f6fb47619b6bc060ff578a18ef39e15cd6c7
David Musuda <dmusuda@gmail.com>
2026-04-24 12:32:54 +0300

    few bugs on auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -42,6 +42,7 @@ export const authOptions: NextAuthOptions = {
           id: user.id,
           email: user.email,
           name: user.name,
+          role: user.role,
         };
       }
     })
